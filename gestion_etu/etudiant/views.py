ae1a8f4b6d3b95cde72e12d870ab86c6eb476f2b
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2025-07-28 15:16:22 +0100

    configuration/affichagee de data sur la page web et insertion dans la db

---
+++ gestion_etu/etudiant/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import Student_registrations
+from .models import User
+
+
+# Create your views here.
+def show(request):
+    if request.method == 'POST':
+        fm =Student_registrations(request.POST)
+        #insertion dans la data base
+        if fm.is_valid():
+            #apres insertion dans la database nettoyer les champs 
+            nettoyer_nom= fm.cleaned_data['name']
+            nettoyer_email= fm.cleaned_data['email']
+            nettoyer_password= fm.cleaned_data['password']
+            reg= User(name=nettoyer_nom, email=nettoyer_email, password=nettoyer_password)
+            reg.save()
+            fm= Student_registrations()
+    else:
+        fm =Student_registrations() 
+    student= User.objects.all()
+    return render(request, 'etudiant/affichage.html', {'form':fm, 'etudiant':student})
